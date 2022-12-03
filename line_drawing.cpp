ea1e8b57dd18bda44b67076ae4fabaa2be76b662
Darshan_kedare <113816565+Darshk03@users.noreply.github.com>
2022-12-03 08:26:11 +0530

    Create line_drawing.cpp

---
+++ line_drawing.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<graphics.h>
+#include<math.h>
+using namespace std;
+
+class draw_line
+{
+	public:
+		int n,x1,y1,x2,y2;
+		void getdata();
+		void display();
+};
+void draw_line :: getdata()
+{	
+	cout << "How many line you have to draw:";
+	cin >>	n;
+		
+	for ( int i = 0 ; i < n ; i++ )
+	{
+		cout <<"Enter the x1,y1,x2,y2 :";
+		cin >> x1 >> y1 >> x2 >> y2;
+	}		
+}
+
+void draw_line :: display()
+{
+line ( x1 , y1 , x2 , y2);
+}
+int main()
+{
+	draw_line l1;
+	l1.getdata();
+	
+	int gd = DETECT , gm;
+	initgraph( &gd , &gm , NULL);
+	
+	l1.display();
+	delay(30000);
+	closegraph();
+	return 0;
+}
